c9bf953b4c30392276047ae410620b7fb271c4b7
Audun Kvasbø <audun@kvasbo.no>
2023-07-03 11:22:07 +0200

    Change test

---
+++ src/test.ts
@@ -0,0 +1,2 @@
+console.log("This is just a smoke test!");
+process.exit(0);
--- src/watcher.ts
+++ src/watcher.ts
@@ -1,14 +1,6 @@
 import { MqttClient } from './Mqtt';
 import { Tibber } from './Tibber';
 
-// End after five seconds if testing
-if (process.argv[2] !== 'test') {
-    console.log('Starting Tibber Watcher');
-    const mqttClient = new MqttClient();
-    new Tibber(mqttClient);
-} else {
-    console.log('This is just a smoke test!');
-    setTimeout(() => {
-        process.exit(0);
-    }, 2000);
-}
+console.log('Starting Tibber Watcher');
+const mqttClient = new MqttClient();
+new Tibber(mqttClient);
