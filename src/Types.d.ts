1f069fe13d0cf3c13b63ab4f8c7e14018f005dd5
Audun Kvasbø <audun@kvasbo.no>
2023-09-09 11:23:01 +0200

    Update Types.d.ts

--- src/Types.d.ts
+++ src/Types.d.ts
@@ -10,6 +10,64 @@ export enum EnergyResolution {
 
 export type Place = 'home' | 'cabin';
 
+export interface PowerStatusForPlace {
+    updated: Date;
+    power: number;
+    accumulatedConsumption: number;
+    accumulatedProduction: number;
+    accumulatedCost: number;
+    minPower: number;
+    averagePower: number;
+    maxPower: number;
+    accumulatedReward: number;
+    powerProduction: number;
+    minPowerProduction: number;
+    maxPowerProduction: number;
+    currentPrice: number;
+    usageForDay: UsageForDay;
+}
+export interface UsageForDay {
+    [key: hours]: {
+        consumption: number;
+        production: number;
+        total: number;
+        price: number;
+    };
+}
+
+// Yeah... I know
+type hours =
+    | 0
+    | 1
+    | 2
+    | 3
+    | 4
+    | 5
+    | 6
+    | 7
+    | 8
+    | 9
+    | 10
+    | 11
+    | 12
+    | 13
+    | 14
+    | 15
+    | 16
+    | 17
+    | 18
+    | 19
+    | 20
+    | 21
+    | 22
+    | 23;
+
+// Hold the current status for a place.
+export interface PowerStatus {
+    home: PowerStatusForPlace;
+    cabin: PowerStatusForPlace;
+}
+
 export interface IConsumption {
     homeId?: string;
     from: string;
